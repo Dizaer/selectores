7f259e0afb3d00da8ccb8671cdcaaaf3e671db74
IE Dev 2 <ie.dev.2@hotmail.com>
2024-10-29 15:44:12 -0700

    265. Formulario reactivo - primer selector

--- src/app/countries/countries.module.ts
+++ src/app/countries/countries.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { SelectorPageComponent } from './pages/selector-page/selector-page.component';
 import { CountriesRoutingModule } from './countries-routing.module';
@@ -12,7 +13,8 @@ import { CountriesRoutingModule } from './countries-routing.module';
   ],
   imports: [
     CommonModule,
-    CountriesRoutingModule
+    CountriesRoutingModule,
+    ReactiveFormsModule
   ]
 })
 export class CountriesModule { }
