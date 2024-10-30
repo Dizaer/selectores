21dcacfa570a76ae9a0f9fc878a30e825c1172ea
IE Dev 2 <ie.dev.2@hotmail.com>
2024-10-30 08:50:57 -0700

    268. Segundo selector anidado - Países

--- src/app/countries/pages/selector-page/selector-page.component.ts
+++ src/app/countries/pages/selector-page/selector-page.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { CountriesService } from '../../services/countries.service';
 import { Region } from '../../interfaces/country.interfaces';
@@ -8,7 +8,7 @@ import { Region } from '../../interfaces/country.interfaces';
   templateUrl: './selector-page.component.html',
   styles: ``
 })
-export class SelectorPageComponent {
+export class SelectorPageComponent implements OnInit {
 
   public myForm: FormGroup = this.fb.group({
     region: ['', Validators.required ],
@@ -22,8 +22,20 @@ export class SelectorPageComponent {
     private countriesService: CountriesService
   ) {}
 
+  ngOnInit(): void {
+    this.onRegionChanged();
+      
+  }
+
   get regions(): Region[] {
     return this.countriesService.regions;
+  }
+
+  private onRegionChanged():void {
+    this.myForm.get('region')!.valueChanges
+    .subscribe( region => {
+      console.log({ region });
+    });
   }
 
 
